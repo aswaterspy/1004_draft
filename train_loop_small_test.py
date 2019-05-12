5c200109e8a015035a7c5797b1f1f6e77e922d7e
aswaterspy <dz584@nyu.edu>
2019-05-12 02:26:46 -0400

    Add Timere

--- train_loop_small_test.py
+++ train_loop_small_test.py
@@ -8,6 +8,7 @@ $ spark-submit baseline.py hdfs:/user/bm106/pub/project/cf_train.parquet hdfs:/p
 
 # We need sys to get the command line arguments
 import sys
+import time
 
 # And pyspark.sql to get the spark session
 from pyspark.sql import SparkSession
@@ -20,6 +21,8 @@ from pyspark.sql.functions import expr
 
 def main(spark, train_data_file, test_data_file, model_file):
 
+    time_a = time.time()
+    start = time_a
 
     training_data = spark.read.parquet(train_data_file)
     training_data.createOrReplaceTempView('training_data')
@@ -31,6 +34,9 @@ def main(spark, train_data_file, test_data_file, model_file):
     #full_history_rows = full_history_rows.sample(False, 0.02)
     training_data = partial_history_rows.sample(0.01)#.union(full_history_rows) 
     print('Finished Sampling!')
+    time_b = time.time()
+    print(time_a - time_b)
+    time_a = time_b
 
     indexer_id = StringIndexer(inputCol="user_id", outputCol="userindex").setHandleInvalid("skip")
     indexer_id_model = indexer_id.fit(training_data)
@@ -46,6 +52,9 @@ def main(spark, train_data_file, test_data_file, model_file):
     testing_data = indexer_item_model.transform(testing_data)
     testing_data = testing_data.select('userindex','itemindex','count')
     print('Finished Indexing!')
+    time_b = time.time()
+    print(time_a - time_b)
+    time_a = time_b
 
     result_dict = {}
     rank_list = [10]
@@ -59,26 +68,40 @@ def main(spark, train_data_file, test_data_file, model_file):
                 als = ALS(maxIter=5, userCol="userindex", itemCol="itemindex", ratingCol="count", rank=rank, regParam=reg_param, alpha=alpha)
                 model = als.fit(training_data)
                 print('Finished Modeling with Param:', current_key)
+                time_b = time.time()
+                print(time_a - time_b)
+                time_a = time_b
 
                 prediction = model.recommendForAllUsers(500).select('userindex', 'recommendations.itemindex')
                 
                 testing_df = testing_data.groupBy('userindex').agg(expr('collect_list(itemindex) as item_list'))
                 predictionAndLabels = prediction.join(testing_df, 'userindex')
                 print('Joined Prediction and Labels!')
+                time_b = time.time()
+                print(time_a - time_b)
+                time_a = time_b
                 pred_df = predictionAndLabels.select(['itemindex','item_list']).rdd.map(list)
 
                 metrics = RankingMetrics(pred_df)
                 print('Ranking Metrics Calculated!')
+                time_b = time.time()
+                print(time_a - time_b)
+                time_a = time_b
                 eva = metrics.meanAveragePrecision
 
                 result_dict[current_key] = eva
 
                 print(current_key,"parameter combination has been trained! MAP= ", eva)
+                time_b = time.time()
+                print(time_a - time_b)
+                time_a = time_b
 
     best_model_param = max(result_dict, key=result_dict.get)
     als = ALS(maxIter=5, userCol="userindex", itemCol="itemindex", ratingCol="count", rank=best_model_param[0], regParam=best_model_param[1], alpha=best_model_param[2])
     als.fit(training_data).write().overwrite().save(model_file)
 
+    print('Process Finished!')
+    print(time.time() - start)
 # Only enter this block if we're in main
 if __name__ == "__main__":
 
